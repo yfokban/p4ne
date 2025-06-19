3b6bd9b3071073ae7d2e7c69a6bec7d8a791fd6e
Yfokban <shulghin.2002@mail.ru>
2025-06-19 17:09:22 +0300

    Complete Lab1.3

---
+++ Lab1.3/Lab1.3.py
@@ -0,0 +1,23 @@
+import pysnmp
+from pysnmp.hlapi import *
+
+result = getCmd(SnmpEngine(),
+                CommunityData("public", mpModel=0),
+                UdpTransportTarget(("10.31.70.209", 161)),
+                ContextData(),
+                ObjectType(ObjectIdentity("SNMPv2-MIB", 'sysDescr', 0)))
+
+for x in result:
+    for y in x[3]:
+        print(y)
+
+result2 = nextCmd(SnmpEngine(),
+                CommunityData("public", mpModel=0),
+                UdpTransportTarget(("10.31.70.209", 161)),
+                ContextData(),
+                ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.2')),
+                lexicographicMode=False)
+
+for x in result2:
+    for y in x[3]:
+        print(y)
